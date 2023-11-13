9a9e75cb5acdcd593bb0d7bfdb7774e480445ac7
Adam Zverbik <zverbik.a@gmail.com>
2023-11-13 19:46:12 +0100

    Add simple vue application

---
+++ todo-items/src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <div id="app">Hello vue.js</div>
+</template>
---
+++ todo-items/webpack.config.js
@@ -0,0 +1,29 @@
+const { VueLoaderPlugin } = require("vue-loader");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/main.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".vue", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+    ],
+  },
+  plugins: [new VueLoaderPlugin()],
+};
